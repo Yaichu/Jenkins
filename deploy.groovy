c2a210e4fca626f54dfedefca0672559fc46d5d5
Yael <yael346@gmail.com>
2020-01-25 17:27:07 +0200

    add stop and rm to the container before starting

--- deploy.groovy
+++ deploy.groovy
@@ -1,5 +1,6 @@
 node("linux") {
     stage('Run container') {
+        sh "docker stop training && docker rm training"
         sh "docker run -d --name training -p 8080:5000 training/webapp"
     }
 }
