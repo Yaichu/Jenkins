424f984ca1b3e19d2589e57de20b58ebda60a4b1
Yael <yael346@gmail.com>
2020-01-29 16:51:09 +0200

    add build stage

--- deploy.groovy
+++ deploy.groovy
@@ -1,6 +1,10 @@
-node("linux") {
-    stage('Run container') {
-        sh "docker stop training && docker rm training"
-        sh "docker run -d --restart always --name training -p 8080:5000 training/webapp"
+node("docker") {
+    stage("build") {
+        customImage = docker.build("proj_app")
     }
+
+    // stage('Run container') {
+    //     sh "docker stop training && docker rm training"
+    //     sh "docker run -d --restart always --name training -p 8080:5000 training/webapp"
+    // }
 }
